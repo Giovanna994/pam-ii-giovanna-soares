3fe6f545c4e16c3e695190493466f805060bf3d0
Giovanna Sousa <161782726+Giovanna994@users.noreply.github.com>
2024-09-01 16:16:06 -0300

    Navegação entre páginas
    Atividade com o objetivo de conseguir navegar entre duas páginas, arquivo inacabado, precisa de mais testes

---
+++ AtividadeDeSegunda/EntregarSegunda/app/routes/SegundoLink.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function SegundoLink(){
+    return(
+        <div>
+            <h1>Atividade de Linkar páginas</h1>
+            <p> Segundo teste, <a href="EntregarSegunda\app\(tabs)\index.tsx"> Voltar </a> </p>     
+        </div>
+    )
+}
+
+export default SegundoLink
